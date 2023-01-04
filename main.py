bbfc6ec37e91689e761e5ac9ed231e80368426b3
Josh Siefert <joshtheman55555@gmail.com>
2023-01-03 23:19:27 -0500

    Finished basic admin commands and started work on voice commands, not finished or working yet.

--- main.py
+++ main.py
@@ -39,18 +39,43 @@ async def clear(ctx, amount=5):
         await ctx.channel.purge(limit=amount)
 
 @bot.command()
-async def kick(ctx, member : discord.Member, *, reason='This action was preformed by a bot at the request of a moderator.'):
+async def kick(ctx, member : discord.Member, *, reason='This action was preformed by a bot at the request of a moderator.',):
+    content=f'You have been kicked from the server with the reason of "{reason}".'
     await member.kick(reason=reason)
+    print(f'Kicked {member} for {reason}.')
+    channel = await member.create_dm()
+    await channel.send(content)
 
 @bot.command()
 async def ban(ctx, member : discord.Member, *, reason='This action was preformed by a bot at the request of a moderator.'):
+    content=f'You have been banned from the server with the reason of "{reason}".'
     await member.ban(reason=reason)
+    print(f'Banned {member} for {reason}.')
+    channel = await member.create_dm()
+    await channel.send(content)
 
 @bot.command()
 async def unban(ctx, *, id: int):
+    content = f'You have been unbanned from {ctx.guild.name}.'
     user = await bot.fetch_user(id)
     await ctx.guild.unban(user)
     await ctx.send(f'Unbanned {user}.')
+    print(f'Unbanned {user}.')
+    channel = await user.create_dm()
+    await channel.send(content)
+
+@bot.command()
+async def nono(ctx):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+    await discord.VoiceClient.play(discord.FFmpegPCMAudio('./assets/nono.mp3'))
+    await discord.VoiceClient.disconnect()
+    
+@bot.command()
+async def disconnect(ctx):
+    await discord.VoiceClient.disconnect()
+
+
 
 ### BOT RUN WITH KEY! DO NOT EDIT ###
 if __name__ == '__main__':
